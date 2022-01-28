7a9f2a5ca6a44d95cf3a27d31bf67fdae4f8401e
vfyjxf <2331007009@qq.com>
2022-01-28 16:59:36 +0800

    use MixinBooster

--- src/main/java/vfyjxf/topfix/TopFix.java
+++ src/main/java/vfyjxf/topfix/TopFix.java
@@ -8,13 +8,12 @@ import java.util.logging.Logger;
         modid = TopFix.MOD_ID,
         name = TopFix.MOD_NAME,
         version = TopFix.VERSION,
-        dependencies = "required-after:theoneprobe"
+        dependencies = "required-after:theoneprobe;required-after:mixinbooter;"
 )
 public class TopFix {
 
     public static final String MOD_ID = "topfix";
     public static final String MOD_NAME = "Top Fix";
     public static final String VERSION = "1.0-SNAPSHOT";
-    public static final Logger logger = Logger.getLogger("topfix");
 
 }
